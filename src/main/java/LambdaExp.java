e32ebfd2c933038a2c9c7e51d9a02a0841209df4
Sean Smith <sean.smith.2009@gmail.com>
2017-11-04 11:56:34 -0700

    Adds lamda expression functional interface example

---
+++ src/main/java/LambdaExp.java
@@ -0,0 +1,23 @@
+
+interface Runner {
+    void run(String name, int days);
+}
+
+class Exec {
+    public void execute(Runner r) {
+        r.run("Sze Wai", 0);
+    }
+}
+
+public class LambdaExp {
+    public static void main(String[] args) {
+        System.out.println("Starting...");
+
+        Exec x = new Exec();
+        x.execute((s, d) -> {
+            System.out.println("Lamdaing: ");
+            System.out.println(s);
+            System.out.println("Days Left: " + d);
+        });
+    }
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,10 +1,11 @@
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("HELLO!");
+        System.out.println("Hello, Sze Wai.");
 
         Counter c = new Counter(60);
         int value = c.getCount();
-
         System.out.println(value);
+
     }
 }
