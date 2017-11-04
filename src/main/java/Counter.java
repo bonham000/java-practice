d22e7ed1ff109d178fd2029636b2ca520fdbde71
Sean Smith <sean.smith.2009@gmail.com>
2017-11-04 11:03:42 -0700

    Adds counter and updates gitignore

---
+++ src/main/java/Counter.java
@@ -0,0 +1,9 @@
+public class Counter {
+    private int count;
+    public Counter(int initialize) {
+        this.count = initialize;
+    }
+    public int getCount() {
+        return this.count;
+    }
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,10 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("HELLO!");
+
+        Counter c = new Counter(60);
+        int value = c.getCount();
+
+        System.out.println(value);
     }
 }
